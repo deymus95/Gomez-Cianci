b4d4e1355dfac24c7184a202c6109473e3bad772
gonzajojo <gonza87.gomez@gmail.com>
2015-09-02 19:30:18 -0300

    test rectangulo

---
+++ PrimeraTarea/src/ar/edu/unlam/tallerweb/cuadrado/Cuadrado.java
@@ -0,0 +1,15 @@
+package ar.edu.unlam.tallerweb.cuadrado;
+
+public class Cuadrado {
+	
+	Integer lado;
+	Integer resultado;
+	
+	public Integer perimetro(Integer lado){
+		
+		return resultado= lado*4;
+	}
+	
+	
+
+}
