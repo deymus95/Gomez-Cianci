274890a2298ddca71e367fd88e8a685d8d3b5958
deymus <deymus95@gmail.com>
2015-09-02 19:03:13 -0300

    import rectangulo

---
+++ Primera tarea/src/ar/edu/unlam/tallerweb/rectangulo/Rectangulo.java
@@ -0,0 +1,12 @@
+package ar.edu.unlam.tallerweb.rectangulo;
+
+public class Rectangulo {
+	Integer ladoancho;
+	Integer ladocorto;
+	Integer resultado;
+
+
+public Integer perimetro(Integer ladoancho, Integer ladocorto){
+	return resultado = (ladoancho*2)+(ladocorto*2);
+}
+}
